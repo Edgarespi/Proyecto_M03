166236c26334502d942ad781671b001b12e264a5
Brunosmx <44584418+Brunosmx@users.noreply.github.com>
2022-05-23 18:03:04 +0200

    Create usuariosSprint2.java
    class de string 2

---
+++ src/proyecto/usuariosSprint2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectom03;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Brandon, Bruno, Edgar
+ */
+public class usuarios implements Serializable {
+    public String rol;
+    public String nombre;
+    public String contraseña;
+}
